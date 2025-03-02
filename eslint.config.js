c5cc8ba6ab569b619b78d356ce378f254a9c5d58
garamnamo <garamnamo@gmail.com>
2025-03-02 21:29:35 +0900

    chore: modify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import * as eslintJs from "@eslint/js";
-import * as eslintPluginJest from "eslint-plugin-jest";
-import * as eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import * as eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintJs from "@eslint/js";
+import eslintPluginJest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import eslintPluginSonarjs from "eslint-plugin-sonarjs";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import typescriptEslint from "typescript-eslint";
